160834a586e3ebb6c6860210d95bfc0549b08286
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2023-05-10 15:35:45 +0530

    Create NQUEEN.cpp

---
+++ NQUEEN.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int x[90];
+bool place(int k,int i)
+{
+    for(int j=1;j<=(k-1);j++)
+    {
+        if(x[j]==i||(abs(x[j]-i)==abs(j-k)))
+        return false;
+    }
+    return true;
+}
+void nqueen(int k,int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        if(place(k,i))
+        {
+            x[k]=i;
+            if(k==n){
+            for(int j=1;j<=n;j++)
+            cout<<x[j];
+            }
+             else
+        nqueen(k+1,n);
+        }
+        
+       
+    }
+}
+int main()
+{
+    nqueen(1,8);
+}
